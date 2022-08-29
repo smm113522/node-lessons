bacb37ab23416d04d52266e2a7005dbe814a5fbd
樂觀 <908085643@qq.com>
2022-08-29 08:14:38 +0800

    x

--- lesson19/app.js
+++ lesson19/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var mysql = require('mysql');
 
-
 var app = express();
 
 app.get('/', function (req, res) {
@@ -9,22 +8,22 @@ app.get('/', function (req, res) {
 })
 
 app.get('/get', function (req, res) {
-    
     var connection = mysql.createConnection({
         host : 'localhost',
         user : 'root',
-        password : '',
-        database : 'test'
+        password : '123456',
+        database : 'demo'
     });
     connection.connect();
     connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
         if (error) throw error;
         console.log('The solution is: ', results[0].solution);
-        res.send('The solution is: ', results[0].solution)
-    });
+        res.send('Hello World:' +  results[0].solution);
+    }); 
+   
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(8082, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
