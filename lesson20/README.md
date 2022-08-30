a7465737421c62598d21508a8f536d75611b384a
smm113522 <908085643@qq.com>
2022-08-30 12:00:13 +0800

    sqitie

--- lesson20/README.md
+++ lesson20/README.md
@@ -2,3 +2,8 @@
 npm install express -save
 
 npm install sqlite3 -save
+
+https://blog.csdn.net/xutongbao/article/details/119570583
+
+code 添加sqlite 插件
+
--- lesson20/app.js
+++ lesson20/app.js
@@ -1,22 +1,36 @@
 var express = require('express');
 // const { runSql, queryPromise } = require('db/index')
 const sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('db/test.db')
 
-
+//执行sql语句
+const runSql = async (sql) => {
+    return new Promise(async (resolve, reject) => {
+      db.run(sql, (err) => {
+        resolve(err)
+      })
+    })
+}
 
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send('Hello World1');
 })
 
-app.get('/get', function (req, res) {
+app.get('/get', function (req, res1) {
     
-    // const result = runSql(
-    //   `SELECT * FROM user`
-    // )
-    // let list = [...result]
-    res.send('Hello World');
+    const TEST_LIST_SQL = `SELECT * FROM user ORDER BY id DESC`;
+    db.all(TEST_LIST_SQL, (err, res2) => {
+    if (err) {
+        console.log('查询数据列表失败::');
+        console.log(err)
+        return;
+    }
+    console.log('查询数据列表成功::');
+    console.log(res2)
+    res1.send('Hello World' + JSON.stringify(res2));
+});
 
 })
 
